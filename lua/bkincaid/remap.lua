2828a7b1c52f0ae7b9aa1a267b280c2d19a787b1
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-03-12 19:15:37 -0700

    add qa remap

--- lua/bkincaid/remap.lua
+++ lua/bkincaid/remap.lua
@@ -4,6 +4,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
 vim.keymap.set("n", "<leader>d", "<cmd>bd<cr>")
 vim.keymap.set("n", "<leader>s", "<cmd>w<cr>")
+vim.keymap.set("n", "<leader>X", "<cmd>qa!<cr>")
+
 
 -- Use ]b and [b to navigate buffers
 vim.keymap.set("n", "]b", "<cmd>bnext<cr>")
