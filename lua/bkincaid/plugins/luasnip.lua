c41544bf81bf41cbbcfcd669fc5b4f6d0387627c
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-04-08 14:18:19 -0700

    add react snippets

--- lua/bkincaid/plugins/luasnip.lua
+++ lua/bkincaid/plugins/luasnip.lua
@@ -6,11 +6,102 @@ local M = {
 	build = "make install_jsregexp"
 }
 
+-- Create snippet object for a React component skeleton
+function CreateReactComponentSnippet(opts)
+	local ls = require("luasnip")
+
+	local text = ls.text_node;
+	local insert = ls.insert_node;
+	local func = ls.function_node;
+	local filename = function()
+		return func(function(_, snip)
+			local name = snip.env.TM_FILENAME
+			return name:match("(.+)%..+$"):gsub("^%l", string.upper)
+		end, {})
+	end
+
+	-- Default user supplied opts to empty table
+	opts = opts or {}
+
+	-- default options for the snippet; will create an "rfce" if no opts provided.
+	local defaultOpts = {
+		ts = false,
+		export = true
+	}
+
+	-- Merge provided options with default
+	local merged_opts = defaultOpts;
+	for k, v in pairs(opts) do merged_opts[k] = v end
+
+	-- The snippet table to be returned
+	local snippet = {}
+
+	-- For ts, output Props type
+	if merged_opts.ts == true then
+		table.insert(snippet, text("export type "))
+		table.insert(snippet, filename())
+		table.insert(snippet, text({ "Props = {}", "", "" }))
+	end
+
+	if merged_opts.export == true then
+		table.insert(snippet, text("export default "))
+	end
+
+	table.insert(snippet, text("function "))
+	table.insert(snippet, filename())
+	table.insert(snippet, text('({}'))
+	-- For ts, define type in parameter
+	if merged_opts.ts == true then
+		table.insert(snippet, text(': '))
+		table.insert(snippet, filename())
+		table.insert(snippet, text('Props'))
+	end
+	table.insert(snippet, text(') '))
+
+	table.insert(snippet, text({ '{', '' }))
+	table.insert(snippet, text('	')) --Indent for newline
+	table.insert(snippet, insert(1))
+	table.insert(snippet, text({ '', '}' }))
+
+	return snippet;
+end
+
 function M.config()
 	local ls = require("luasnip")
 
-	ls.add_snippets(nil, {
-		all = {
+	-- JS/JSX Snippets
+	ls.add_snippets("javascript", {
+		ls.snippet({
+			trig = "rfce",
+			namr = "react_js_component_w_props_export",
+			dscr = "Create a React component (Javascript) with an interface for props. Component is exported."
+		}, CreateReactComponentSnippet({ ts = false, export = true })),
+		ls.snippet({
+			trig = "rfc",
+			namr = "react_js_component_w_props",
+			dscr = "Create a React component (Javascript) with an interface for props."
+		}, CreateReactComponentSnippet({ ts = false, export = false })),
+	})
+	ls.filetype_extend("javascriptreact", { "javascript" })
+
+	-- TS/TSX Snippets
+	ls.add_snippets("typescript", {
+		ls.snippet({
+			trig = "trfce",
+			namr = "react_ts_component_w_props_export",
+			dscr = "Create a React component (Typescript) with an interface for props. Component & types are exported."
+		}, CreateReactComponentSnippet({ ts = true, export = true })),
+		ls.snippet({
+			trig = "trfc",
+			namr = "react_ts_component_w_props",
+			dscr = "Create a React component (Typescript) with an interface for props."
+		}, CreateReactComponentSnippet({ ts = true, export = false })),
+	})
+	ls.filetype_extend("typescriptreact", { "typescript" })
+
+	-- MD Snippets
+	ls.add_snippets("markdown",
+		{
 			ls.snippet({
 				trig = "link",
 				namr = "markdown_link",
@@ -26,7 +117,7 @@ function M.config()
 				ls.insert_node(0),
 			})
 		}
-	});
+	);
 end
 
 return M
