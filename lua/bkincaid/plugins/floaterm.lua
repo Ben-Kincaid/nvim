fc5aa769432d969228e225693c0d92fddbd5a975
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-04-13 00:16:23 -0700

    disabling lazygit integration for right now

--- lua/bkincaid/plugins/floaterm.lua
+++ lua/bkincaid/plugins/floaterm.lua
@@ -5,7 +5,9 @@ local test = "wow"
 print(test)
 
 function M.config()
-		vim.keymap.set("n", "<leader>tg", ":FloatermNew --width=0.85 --height=0.85 lazygit<CR>", { noremap = true, silent = true })
+	-- Let's revisit this; fugitive + gitsigns workflow is pretty nice
+	-- Lazygit seems great but i don't like having different keybindings for something that behaves like a nvim plugin with floaterm
+	-- vim.keymap.set("n", "<leader>tg", ":FloatermNew --width=0.85 --height=0.85 lazygit<CR>", { noremap = true, silent = true })
 end
 
 
