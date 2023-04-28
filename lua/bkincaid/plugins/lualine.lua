82c739e712d545e073ca58ff66278870b2761602
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-04-28 01:57:43 -0700

    cleanup status line

--- lua/bkincaid/plugins/lualine.lua
+++ lua/bkincaid/plugins/lualine.lua
@@ -1,35 +1,37 @@
 local M = {
-	'nvim-lualine/lualine.nvim'
+  'nvim-lualine/lualine.nvim'
 }
 
 
 function M.config()
-	local disabled_filetypes = { 'packer', 'NvimTree', 'alpha' }
-	local disabled_win_filetypes = { 'packer', 'NvimTree', 'alpha', 'gitcommit', 'help', 'fugitive' }
+  local disabled_filetypes = { 'packer', 'NvimTree', 'alpha' }
+  local disabled_win_filetypes = { 'packer', 'NvimTree', 'alpha', 'gitcommit', 'help', 'fugitive' }
 
-	require('lualine').setup({
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { { 'filename', file_status = true, newfile_status = false, path = 1 } },
-			lualine_c = { { 'filename', path = 0 } },
-			lualine_x = { 'filetype' },
-			lualine_y = { 'progress' },
-			lualine_z = { 'location' }
-		},
-		options = {
-			disabled_filetypes = {
-				statusline = disabled_filetypes,
-				winbar = disabled_win_filetypes,
-				inactive_winbar = disabled_win_filetypes
-			}
-		},
-		winbar = {
-			lualine_c = { { 'filename', file_status = true, path = 1 } }
-		},
-		inactive_winbar = {
-			lualine_c = { { 'filename', file_status = true, path = 1 } }
-		}
-	})
+
+  require('lualine').setup({
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { { 'filename', path = 0 } },
+      lualine_c = { 'diff', 'diagnostics' },
+      lualine_x = { 'branch', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+    options = {
+      disabled_filetypes = {
+        statusline = disabled_filetypes,
+        winbar = disabled_win_filetypes,
+        inactive_winbar = disabled_win_filetypes
+      }
+    },
+    winbar = {
+      lualine_c = { { 'filename', file_status = true, path = 1 } }
+    },
+    inactive_winbar = {
+      lualine_c = { { 'filename', file_status = true, path = 1 } }
+    },
+    extensions = { 'fugitive', 'quickfix' }
+  })
 end
 
 return M
