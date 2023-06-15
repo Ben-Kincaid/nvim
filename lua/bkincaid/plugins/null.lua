f623abc3e038c3e4308f35d352e276367acdabac
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-06-15 01:11:56 -0700

    Use builtin prettierd formatter vs prettier.nvim, remove BufWritePre fmt

--- lua/bkincaid/plugins/null.lua
+++ lua/bkincaid/plugins/null.lua
@@ -1,70 +1,21 @@
 local M = {
-	'jose-elias-alvarez/null-ls.nvim',
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/prettier.nvim"
-	}
+  'jose-elias-alvarez/null-ls.nvim',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  }
 }
 
-
 function M.config()
-	-- ## null-ls: Supporting lagnuages that dont follow LSP ##"camera_offset"   "226.06 73.87 64.82"
+  -- ## null-ls: Supporting languages that dont provide an actual LSP server
   local null = require("null-ls");
-	local lsp_formatting = function(bufnr)
-		vim.lsp.buf.format({
-			filter = function(client)
-				-- apply whatever logic you want (in this example, we'll only use null-ls)
-				return client.name == "null-ls"
-			end,
-			bufnr = bufnr,
-		})
-	end
 
-	-- if you want to set up formatting on save, you can use this as a callback
-	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-	-- add to your shared on_attach callback
-	local on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					lsp_formatting(bufnr)
-				end,
-			})
-		end
-	end
-
-	require("null-ls").setup({
+  null.setup({
     sources = {
       null.builtins.formatting.pint,
-      null.builtins.formatting.phpcsfixer
-    },
-		on_attach = on_attach
-	})
-
-	-- ## Setup prettier.nvim; integrates prettier in LSP with null-ls
-	local prettier = require("prettier");
-	prettier.setup({
-		bin = 'prettierd', -- or `'prettierd'` (v0.23.3+)
-		filetypes = {
-			"css",
-			"graphql",
-			"html",
-			"javascript",
-			"javascriptreact",
-			"json",
-			"less",
-			"markdown",
-			"scss",
-			"typescript",
-			"typescriptreact",
-			"yaml",
-			"svelte"
-		},
-	})
+      null.builtins.formatting.phpcsfixer,
+      null.builtins.formatting.prettierd,
+    }
+  })
 end
 
 return M
