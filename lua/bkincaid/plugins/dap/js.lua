ad21aa7e77b20c730f76f32e92d346f706a76352
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-06-29 23:48:18 -0700

    Add chrome debugger config

--- lua/bkincaid/plugins/dap/js.lua
+++ lua/bkincaid/plugins/dap/js.lua
@@ -12,7 +12,7 @@ local M = {
 function M.config()
   require("dap-vscode-js").setup({
     debugger_path = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug",
-    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+    adapters = { 'pwa-node', 'pwa-chrome' }
   })
 
   for _, language in ipairs({ "typescript", "typescriptreact", "javascript", "javascriptreact" }) do
@@ -20,17 +20,24 @@ function M.config()
       {
         type = "pwa-node",
         request = "launch",
-        name = "Launch file",
+        name = "Launch file (Node)",
         program = "${file}",
         cwd = "${workspaceFolder}",
       },
       {
+        type = "pwa-chrome",
+        request = "launch",
+        name = "Launch (Chrome)",
+        url = "http://localhost:3000",
+        webRoot = "${workspaceFolder}",
+      },
+      {
         type = "pwa-node",
         request = "attach",
-        name = "Attach",
+        name = "Attach to Process (Node)",
         processId = require 'dap.utils'.pick_process,
         cwd = "${workspaceFolder}",
-      }
+      },
     }
   end
 end
