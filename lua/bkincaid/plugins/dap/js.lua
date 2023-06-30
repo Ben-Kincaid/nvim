fc7fd86c73b7ed840723052a596d6cc6ec781505
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-06-29 20:43:50 -0700

    Add dap dir (initialization + js config)

---
+++ lua/bkincaid/plugins/dap/js.lua
@@ -0,0 +1,38 @@
+local M = {
+  "mxsdev/nvim-dap-vscode-js",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    {
+      "microsoft/vscode-js-debug",
+      build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+    }
+  },
+}
+
+function M.config()
+  require("dap-vscode-js").setup({
+    debugger_path = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug",
+    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+  })
+
+  for _, language in ipairs({ "typescript", "javascript" }) do
+    require("dap").configurations[language] = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach",
+        processId = require 'dap.utils'.pick_process,
+        cwd = "${workspaceFolder}",
+      }
+    }
+  end
+end
+
+return M
