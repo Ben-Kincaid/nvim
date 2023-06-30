3207424afd0fd00f5039b45ae99fcdaeea3c7358
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-06-29 20:44:14 -0700

    Disable dap-repl for copilot

--- lua/bkincaid/plugins/copilot.lua
+++ lua/bkincaid/plugins/copilot.lua
@@ -1,8 +1,13 @@
 local M = {
-	'github/copilot.vim'
+  'github/copilot.vim'
 }
 
 function M.config()
+  vim.g.copilot_filetypes = {
+    -- Disable copilot in nvim-dap repl
+    -- https://github.com/neovim/neovim/issues/17614
+    ['dap-repl'] = false
+  }
 end
 
 return M
