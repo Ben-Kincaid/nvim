88e7172a688a81c95ccbfd7a574db64111889c14
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-06-30 00:05:44 -0700

    Add custom chrome adapter to ask for port before launching chrome instance

--- lua/bkincaid/plugins/dap/js.lua
+++ lua/bkincaid/plugins/dap/js.lua
@@ -10,13 +10,33 @@ local M = {
 }
 
 function M.config()
+  local dap = require('dap')
+
+  -- Set up vscode-js-debug adapters
   require("dap-vscode-js").setup({
     debugger_path = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug",
     adapters = { 'pwa-node', 'pwa-chrome' }
   })
 
+
+  -- Extend pwa-chrome to ask for the port before opening the Chrome instance
+  local custom_chrome_adapter = 'pwa-chrome-custom'
+  dap.adapters[custom_chrome_adapter] = function(_, config)
+    vim.ui.input({
+      prompt = "Which port? (3000)"
+    }, function(port)
+      port = port or 3000
+      config.url = "http://localhost:" .. port
+      config.type = 'pwa-chrome'
+      dap.run(config)
+    end)
+  end
+
+
+  -- Register JS/TS configurations
   for _, language in ipairs({ "typescript", "typescriptreact", "javascript", "javascriptreact" }) do
     require("dap").configurations[language] = {
+      -- Run the current buffer via node in inspect mode
       {
         type = "pwa-node",
         request = "launch",
@@ -24,13 +44,15 @@ function M.config()
         program = "${file}",
         cwd = "${workspaceFolder}",
       },
+      -- Launch a dedicated chrome instance w/ debugging enabled
       {
-        type = "pwa-chrome",
+        type = custom_chrome_adapter,
         request = "launch",
         name = "Launch (Chrome)",
         url = "http://localhost:3000",
         webRoot = "${workspaceFolder}",
       },
+      -- Attach to a running node process
       {
         type = "pwa-node",
         request = "attach",
