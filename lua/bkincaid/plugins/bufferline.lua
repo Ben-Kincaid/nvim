7e490439437afacdda130247aa5a6725915cf7d8
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2023-07-11 03:25:39 -0700

    Switch to nightfox, clean up nvimtree config/fillchars

--- lua/bkincaid/plugins/bufferline.lua
+++ lua/bkincaid/plugins/bufferline.lua
@@ -1,24 +1,23 @@
 local M = {
-	'akinsho/bufferline.nvim',
-	version = "v3.*",
-	dependencies = { 'nvim-tree/nvim-web-devicons' }
+  'akinsho/bufferline.nvim',
+  version = "v3.*",
+  dependencies = { 'nvim-tree/nvim-web-devicons' }
 }
 
 
 function M.config()
-	require("bufferline").setup {
-		options = {
-			separator_style = "slant",
-			offsets = {
-				{
-					filetype = "NvimTree",
-					text = "File Explorer",
-					separator = true,
-				}
-			}
-		}
-	}
+  require("bufferline").setup {
+    options = {
+      separator_style = "slant",
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "",
+          separator = true,
+        }
+      }
+    }
+  }
 end
-
 
 return M
--- lua/bkincaid/plugins/nvimtree.lua
+++ lua/bkincaid/plugins/nvimtree.lua
@@ -1,30 +1,30 @@
 local M = {
-	'nvim-tree/nvim-tree.lua',
-	dependencies = {
-		'nvim-tree/nvim-web-devicons', -- optional, for file icons
-	},
-	lazy = false,
-	priority = 999,
-	version = 'nightly' -- optional, updated every week. (see issue #1193)
+  'nvim-tree/nvim-tree.lua',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  lazy = false,
+  priority = 999,
+  version = 'nightly' -- optional, updated every week. (see issue #1193)
 }
 
 
 function M.config()
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
 
-	-- Follow current buffer in file tree
-	vim.g.nvim_tree_follow = 1
+  -- Follow current buffer in file tree
+  vim.g.nvim_tree_follow = 1
 
-	-- empty setup using defaults
-	require("nvim-tree").setup({
-		update_focused_file = {
-			enable = true,
-			ignore_list = { "help" },
-		}
-	})
+  -- empty setup using defaults
+  require("nvim-tree").setup({
+    update_focused_file = {
+      enable = true,
+      ignore_list = { "help" },
+    }
+  })
 
-	vim.keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
+  vim.keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
 end
 
 return M
