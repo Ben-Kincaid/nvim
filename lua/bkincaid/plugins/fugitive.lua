94c5b677eac3aee86c3e87fbbe5b64d1191b9612
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2024-03-28 01:09:41 -0700

    fix: add single git diff tool, better messaging

--- lua/bkincaid/plugins/fugitive.lua
+++ lua/bkincaid/plugins/fugitive.lua
@@ -2,16 +2,26 @@ local M = { 'tpope/vim-fugitive' }
 
 
 function M.config()
-	vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+  vim.keymap.set("n", "<leader>g", vim.cmd.Git)
 
-	-- View git files
-	vim.keymap.set('n', '<leader>G', function()
-		vim.ui.input({
-			prompt = 'Compare with target branch/commit > '
-		}, function(input)
-			vim.cmd(":Git difftool -y " .. input);
-		end)
-	end)
+  -- View diff between current buffer on another branch/commit.
+  vim.keymap.set('n', '<leader><c-g>', function()
+    vim.ui.input({
+      prompt = '[Current File] Compare with target branch/commit > '
+    }, function(input)
+      vim.cmd(":Gvdiff " .. input);
+    end)
+  end)
+
+  -- View diff between current working tree and branch/commit (a la Pull Request)
+  -- Using difftool, each file diff will be opened in a new tab
+  vim.keymap.set('n', '<leader>G', function()
+    vim.ui.input({
+      prompt = '[Current Branch] Compare with target branch/commit > '
+    }, function(input)
+      vim.cmd(":Git difftool -y " .. input);
+    end)
+  end)
 end
 
 return M
