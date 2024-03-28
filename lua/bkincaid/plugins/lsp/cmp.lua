88f9f93faf76d4515d1bddb1d6d8eca526406e8b
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2024-03-28 01:33:37 -0700

    feat: add pictograms + border support for completion/documentation

--- lua/bkincaid/plugins/lsp/cmp.lua
+++ lua/bkincaid/plugins/lsp/cmp.lua
@@ -8,14 +8,33 @@ local M = {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp',
     'saadparwaiz1/cmp_luasnip',
+    -- Pictograms for completion items
+    'onsails/lspkind.nvim'
   }
 }
-
 
 function M.config()
   local cmp = require('cmp')
 
   cmp.setup({
+    window = {
+      documentation = cmp.config.window.bordered(),
+      completion = cmp.config.window.bordered({
+        col_offset = -3,
+        side_padding = 0,
+      }),
+    },
+    formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+        local strings = vim.split(kind.kind, "%s", { trimempty = true })
+        kind.kind = " " .. (strings[1] or "") .. " "
+        kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+        return kind
+      end,
+    },
     snippet = {
       expand = function(args)
         require 'luasnip'.lsp_expand(args.body)
